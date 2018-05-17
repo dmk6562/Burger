118ffea6291ea189aaa6fbe3b4ad232383dd5d79
Catherine Muthoni <cdmuthoni2005@yahoo.com>
2018-05-16 22:51:47 -0400

    initial files

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+ id INTEGER AUTO_INCREMENT,
+ burger_name VARCHAR(60),
+ devoured BOOLEAN,
+ PRIMARY KEY (id)
+ 
+);
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Lamb Burger", true), ("Curry Burger", false), ("Sirloin Burger", true),
+("Green-Chile Burger", true), ("Portobello-Mushroom Burger", false), ("Salmon Burger", true);
+
+SELECT * FROM burgers;
