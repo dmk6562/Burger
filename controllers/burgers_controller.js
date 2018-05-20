11b2237679dcc1b34f6d64d630ec1ee958d3ec95
Catherine Muthoni <cdmuthoni2005@yahoo.com>
2018-05-20 13:20:44 -0400

    edits

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get('/', function (req, res) {
-  res.render('/index');
+  res.redirect('/index');
 });
 
 // Index Page (displays all burgers)
