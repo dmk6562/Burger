a2e4f03a4a4d3f05c4fe124b1916f51f535b7d6e
Catherine Muthoni <cdmuthoni2005@yahoo.com>
2018-05-20 17:30:55 -0400

    'edits'

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,12 +11,12 @@ router.get('/', function (req, res) {
 });
 
 // Index Page (displays all burgers)
-router.get('/index', function (req, res) {
+router.get('../views/index', function (req, res) {
   burger.selectAll(function(data) {
       var hbsObject = { burgers: data };
 
       console.log(hbsObject);
-      res.render('index', hbsObject);
+      res.render('../views/index', hbsObject);
   });
 });
 
@@ -37,7 +37,7 @@ router.get('/index', function (req, res) {
 //Creates a new burger
 router.post('/burger/create', function (req, res) {
   burger.insertOne(req.body.burger_name, function() {
-      res.redirect('/index');
+      res.redirect('../views/index');
   });
 });
 
@@ -45,7 +45,7 @@ router.post('/burger/create', function (req, res) {
 //Devours a Burger
 router.post('/burger/eat/:id', function (req, res) {
   burger.updateOne(req.params.id, function() {
-      res.redirect('/index');
+      res.redirect('../views/index');
   });
 });
 
