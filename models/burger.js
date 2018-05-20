fda6f508ddf22dd02ead359441ae308b8c19bdd1
Catherine Muthoni <cdmuthoni2005@yahoo.com>
2018-05-20 14:03:02 -0400

    edits

--- models/burger.js
+++ models/burger.js
@@ -3,12 +3,12 @@ var orm = require("../config/orm.js");
 
 var burger = {
   selectAll: function(cb) {
-    orm.selectAll(function(req, res) {
+    orm.selectAll(function(res) {
       cb(res);
     });
   },
   insertOne: function(burger_name, cb){
-    orm.insertOne('burger_name', function(req, res){
+    orm.insertOne('burger_name', function(res){
         cb(res);
     });
 },
